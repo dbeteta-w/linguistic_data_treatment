681b7112ab3e4720876dd874a449503ced3d50ef
dbeteta-w <dbeteta.work@gmail.com>
2022-05-17 17:55:31 +0200

    Add extra punctuation to be removed

--- helpers/remove_punctuation.py
+++ helpers/remove_punctuation.py
@@ -2,6 +2,10 @@ import string
 
 
 def remove_punctuation(text: str) -> str:
-    for punct in string.punctuation:
+    extra_punctuation = ["¿", "¡"]
+    punctuation = list(string.punctuation)
+    punctuation.extend(extra_punctuation)
+
+    for punct in punctuation:
         text = text.replace(punct, "")
     return text
