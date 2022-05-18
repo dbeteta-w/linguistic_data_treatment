1ff43ad9c37bd1968df6bac7b9980f9bb050faa0
dbeteta-w <dbeteta.work@gmail.com>
2022-05-18 21:32:00 +0200

    Add new validator process

---
+++ validators/has_parallel_symbols_val.py
@@ -0,0 +1,9 @@
+
+def has_parallel_symbols_val(src: str, tgt: str, tolerance=0, symbols_to_check=None):
+    if symbols_to_check is None:
+        symbols_to_check = {'[', ']', '{', '}', '<', '>', '@', '+', '...', '#'}
+
+    for symbol in symbols_to_check:
+        if abs(src.count(symbol) - tgt.count(symbol)) > tolerance:
+            return False
+    return True
