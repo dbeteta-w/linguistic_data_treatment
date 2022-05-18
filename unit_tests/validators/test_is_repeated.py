6656bea83db8cddf30ad18c4513f3a9ad4cfa5c7
dbeteta-w <dbeteta.work@gmail.com>
2022-05-18 18:01:44 +0200

    Test to check the desired functionality of "is_repeated"

---
+++ unit_tests/validators/test_is_repeated.py
@@ -0,0 +1,27 @@
+import unittest
+
+from validators.is_repeated import is_repeated
+
+
+class TestIsRepeated(unittest.TestCase):
+    def test_validator(self):
+        test_set = {
+            tuple(["holaquetal", "hihowareyou"])
+        }
+
+        test_golds = {
+            tuple(["Hola que tal", "Hi how are you doing"]): False,
+            tuple(["Hola que tal", "Hi how are you"]): True,
+            tuple(["Hola, que tal?", "Hi, how are you?"]): True,
+            tuple(["HOLA QUE TAL", "HI HOW ARE YOU"]): True,
+            tuple([" Hola que     tal", " Hi how    are you"]): True
+        }
+
+        for test, gold in test_golds.items():
+            src, tgt = test
+            x = is_repeated(src, tgt, test_set)
+            self.assertEqual(is_repeated(src, tgt, test_set), gold)
+
+
+if __name__ == '__main__':
+    unittest.main()
