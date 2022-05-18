4cfd1f2b4748171552e28e724f24d38aa85a218e
dbeteta-w <dbeteta.work@gmail.com>
2022-05-18 19:05:20 +0200

    Test to check the proper functionality of has_properly_length_factor

---
+++ unit_tests/validators/test_has_properly_length_factor_val.py
@@ -0,0 +1,21 @@
+import unittest
+
+from validators.has_properly_length_factor_val import has_properly_length_factor_val
+
+
+class TestHasProperlyLengthFactorVal(unittest.TestCase):
+    def test_validator(self):
+        test_golds = {
+            tuple(["Hola", "Hello"]): True,
+            tuple(["Hi", "Hello"]): True,
+            tuple(["Hola", "Hello, how are you doing my friend?"]): False,
+            tuple(["Hola, ¿cómo vas amigo mio?", "Hello"]): False,
+        }
+
+        for test, gold in test_golds.items():
+            src, tgt = test
+            self.assertEqual(has_properly_length_factor_val(src, tgt), gold)
+
+
+if __name__ == '__main__':
+    unittest.main()
