81d7411889bbebecb6d87438a1f928064a7e93c8
dbeteta-w <dbeteta.work@gmail.com>
2022-05-18 20:07:28 +0200

    Add tolerance to the process

--- validators/has_parallel_number_val.py
+++ validators/has_parallel_number_val.py
@@ -1,12 +1,12 @@
 from text_to_num import alpha2digit
 
 
-def has_parallel_number_val(src: str, tgt: str, src_lang: str, tgt_lang: str) -> bool:
+def has_parallel_number_val(src: str, tgt: str, src_lang: str, tgt_lang: str, tolerance=0) -> bool:
     src_alpha2digit = alpha2digit(src, src_lang)
     tgt_alpha2digit = alpha2digit(tgt, tgt_lang)
     for number in range(10):
-        number_to_be_counted = str(number)
-        if src.count(number_to_be_counted) != tgt.count(number_to_be_counted) and \
-                src_alpha2digit.count(number_to_be_counted) != tgt_alpha2digit.count(number_to_be_counted):
+        string_number = str(number)
+        if abs(src.count(string_number) - tgt.count(string_number)) > tolerance and \
+                abs(src_alpha2digit.count(string_number) - tgt_alpha2digit.count(string_number)) > tolerance:
             return False
     return True
