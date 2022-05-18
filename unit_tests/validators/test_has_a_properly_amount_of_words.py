a56f0100258977ba64e02523d20f8ebea235e44d
dbeteta-w <dbeteta.work@gmail.com>
2022-05-18 11:14:06 +0200

    Test to check the process "has_a_properly_amount_of_words"

---
+++ unit_tests/validators/test_has_a_properly_amount_of_words.py
@@ -0,0 +1,25 @@
+import unittest
+
+from validators.has_a_properly_amount_of_words import has_text_properly_amount_of_words
+
+
+class TestHasAProperlyAmountOfWords(unittest.TestCase):
+    def test_validator(self):
+        test_golds = {
+            "Hola que tal": True,
+            "Hola": False,
+            "Os recordamos que \n vuestras opiniones son imprescindibles para mejorar la movilidad sostenible de los "
+            "campus de la UPV. Por este motivo, os invitamos a participar en una encuesta que nos ayudará a conocer "
+            "el sistema de movilidad actual, su evolución y la percepción que de él tiene la comunidad "
+            "universitaria.": False,
+            "Hola     ": False,
+            "     Hola": False,
+            "": False
+        }
+
+        for test, gold in test_golds.items():
+            self.assertEqual(has_text_properly_amount_of_words(test), gold)
+
+
+if __name__ == '__main__':
+    unittest.main()
