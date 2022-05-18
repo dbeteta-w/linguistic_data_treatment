3f6a521300f77b196a1e08a81a6ced32d8178dd9
dbeteta-w <dbeteta.work@gmail.com>
2022-05-18 11:13:21 +0200

    Improve of the aesthetic of the code

--- helpers/clean_to_be_compared.py
+++ helpers/clean_to_be_compared.py
@@ -1,8 +1,18 @@
 import re
 
+from past.builtins import apply
+
 from helpers.remove_punctuation import remove_punctuation
 
 
-def clean_to_be_compared(text: str):
-    text = remove_punctuation(text)
-    return re.sub(r"\s", "", text).casefold()
+def clean_to_be_compared(text: str) -> str:
+    list_of_cleaning_processes = [
+        remove_punctuation,
+        lambda x: x.casefold(),
+        lambda x: re.sub(r"\s", "", x),
+    ]
+
+    for funct in list_of_cleaning_processes:
+        text = apply(funct, text)
+
+    return text
--- validators/has_a_properly_amount_of_words.py
+++ validators/has_a_properly_amount_of_words.py
@@ -1,17 +1,27 @@
 import re
 
+from past.builtins import apply
+
 from helpers.remove_punctuation import remove_punctuation
 
 
 def has_a_properly_amount_of_words(src: str, tgt: str, min_words=2, max_words=35) -> bool:
-    return _has_a_properly_amount_of_words(src, min_words, max_words) \
-           and _has_a_properly_amount_of_words(tgt, min_words, max_words)
+    return has_text_properly_amount_of_words(src, min_words, max_words) \
+           and has_text_properly_amount_of_words(tgt, min_words, max_words)
 
 
-def _has_a_properly_amount_of_words(text: str, min_words=2, max_words=35) -> bool:
-    text = remove_punctuation(text)
-    text_with_normalized_spaces = re.sub(r"[ ]{2,}", " ", text).strip()
-    list_of_words = text_with_normalized_spaces.split(" ")
-    if len(list_of_words) > max_words or len(list_of_words) < min_words:
+def has_text_properly_amount_of_words(text: str, min_words=2, max_words=35) -> bool:
+    list_of_cleaning_processes = [
+        remove_punctuation,
+        lambda x: re.sub(r"\s+", " ", x),
+        lambda x: x.strip()
+    ]
+
+    for funct in list_of_cleaning_processes:
+        text = apply(funct, text)
+
+    list_of_words = text.split(" ")
+    len_list_of_words = len(list_of_words)
+    if len_list_of_words > max_words or len_list_of_words < min_words:
         return False
     return True
