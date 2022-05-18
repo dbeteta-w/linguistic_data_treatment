25c8f56f408ba346714e5523dc178a20658f5ddd
dbeteta-w <dbeteta.work@gmail.com>
2022-05-18 11:33:34 +0200

    Rename of a function

--- validators/has_too_many_numbers.py
+++ validators/has_too_many_numbers.py
@@ -2,11 +2,11 @@ import re
 
 
 def has_too_many_numbers(src: str, tgt: str, alpha=2) -> bool:
-    return not _has_too_many_numbers(src, alpha) \
-           and not _has_too_many_numbers(tgt, alpha)
+    return not has_text_too_many_numbers(src, alpha) \
+           and not has_text_too_many_numbers(tgt, alpha)
 
 
-def _has_too_many_numbers(text: str, alpha=2) -> bool:
+def has_text_too_many_numbers(text: str, alpha=2) -> bool:
     list_of_numbers = re.findall(r"\d", text)
     list_of_letters = re.findall(r"[A-Za-z]", text)
     if len(list_of_numbers) * alpha >= len(list_of_letters):
