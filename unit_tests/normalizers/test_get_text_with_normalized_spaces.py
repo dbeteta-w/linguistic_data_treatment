872f348edb78bdb58d16cbb1ba154f875085b43d
dbeteta-w <dbeteta.work@gmail.com>
2022-05-20 16:45:36 +0200

    Test to check the proper functionality of "get_text_with_normalized_spaces"

---
+++ unit_tests/normalizers/test_get_text_with_normalized_spaces.py
@@ -0,0 +1,23 @@
+import unittest
+
+from normalizers.get_text_with_normalized_spaces import get_text_with_normalized_spaces
+
+
+class TestGetTextWithNormalizedSpaces(unittest.TestCase):
+    def test_normalizer(self):
+        test_golds = {
+            "Hola que tal": "Hola que tal",
+            "Hola    que tal": "Hola que tal",
+            "Hola que\t tal": "Hola que tal",
+            "Hola que tal\n": "Hola que tal",
+            "Hola   ": "Hola",
+            "   Hola": "Hola"
+        }
+
+        for test, gold in test_golds.items():
+            text_with_normalized_spaces = get_text_with_normalized_spaces(test)
+            self.assertEqual(text_with_normalized_spaces, gold)
+
+
+if __name__ == '__main__':
+    unittest.main()
