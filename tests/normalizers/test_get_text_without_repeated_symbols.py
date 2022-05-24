314309e3e52504e9efee330fb34e7febe5cd4c79
dbeteta-w <dbeteta.work@gmail.com>
2022-05-24 22:18:02 +0200

    Test to check the proper functionality of "get_text_without_repeated_symbols"

---
+++ tests/normalizers/test_get_text_without_repeated_symbols.py
@@ -0,0 +1,24 @@
+import unittest
+
+from processes.normalizers.get_text_without_repeated_symbols import get_text_without_repeated_symbols
+
+
+class TestGetTextWithoutRepeatedSymbols(unittest.TestCase):
+    def test_normalizer(self):
+        test_golds = {
+            "Hola que tal": "Hola que tal",
+            "Hola,, que tal": "Hola, que tal",
+            "Hola que tal..": "Hola que tal.",
+            "Hola que tal;.": "Hola que tal;",
+            "Hola,:, que tal": "Hola, que tal",
+            "Hola,, que tal??": "Hola, que tal?"
+        }
+
+        for test, gold in test_golds.items():
+            test_without_repeated_symbols = get_text_without_repeated_symbols(test)
+            self.assertEqual(test_without_repeated_symbols, gold)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
