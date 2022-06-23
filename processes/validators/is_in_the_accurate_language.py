80506995c03de2edf1e57f1955d5f504b1ccbb32
dbeteta-w <dbeteta.work@gmail.com>
2022-06-23 12:58:00 +0200

    Refactor

--- processes/validators/is_in_the_accurate_language.py
+++ processes/validators/is_in_the_accurate_language.py
@@ -8,19 +8,20 @@ def is_in_the_accurate_language(src: str, src_lang: str, tgt: str, tgt_lang: str
 
 
 def is_text_in_the_accurate_language(text: str, lang: str, fasttext_model, minimum_probability=0.4) -> bool:
-    amount_of_guesses = 2
+    AMOUNT_OF_GUESSES = 2
+    LABEL_TYPO = "__label__"
     text_with_normalized_spaces = get_text_with_normalized_spaces(text)
 
-    guess_array = fasttext_model.predict(text_with_normalized_spaces, k=amount_of_guesses)
-    if len(guess_array) >= amount_of_guesses \
-            and len(guess_array[0]) >= amount_of_guesses \
-            and len(guess_array[1]) >= amount_of_guesses:
-        first_lang_guess = guess_array[0][0].replace("__label__", "")
+    guess_array = fasttext_model.predict(text_with_normalized_spaces, k=AMOUNT_OF_GUESSES)
+    if len(guess_array) >= AMOUNT_OF_GUESSES \
+            and len(guess_array[0]) >= AMOUNT_OF_GUESSES \
+            and len(guess_array[1]) >= AMOUNT_OF_GUESSES:
+        first_lang_guess = guess_array[0][0].replace(LABEL_TYPO, "")
         first_lang_prob = guess_array[1][0]
         if first_lang_guess != lang or \
                 (first_lang_guess == lang and
                  first_lang_prob < minimum_probability):
-            second_lang_guess = guess_array[0][1].replace("__label__", "")
+            second_lang_guess = guess_array[0][1].replace(LABEL_TYPO, "")
             second_lang_prob = guess_array[1][1]
             if second_lang_guess != lang or \
                     (second_lang_guess == lang and
